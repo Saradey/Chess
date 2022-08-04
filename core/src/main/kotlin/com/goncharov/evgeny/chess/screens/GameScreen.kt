5126bb848219e1a991bbf2a2c7ca412d3dac6cc7
Evgeny Goncharov <jtgn@yandex.ru>
2022-08-05 00:52:18 +0300

    добавили базовый код

--- core/src/main/kotlin/com/goncharov/evgeny/chess/screens/GameScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/screens/GameScreen.kt
@@ -1,7 +1,32 @@
 package com.goncharov.evgeny.chess.screens
 
 import com.goncharov.evgeny.chess.base.BaseScreen
+import com.goncharov.evgeny.chess.utils.clearScreen
+import com.goncharov.evgeny.chess.utils.debug
 
 class GameScreen : BaseScreen() {
 
+    override fun show() {
+        debug(TAG, "show()")
+    }
+
+    override fun render(delta: Float) {
+        clearScreen()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        debug(TAG, "resize()")
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+        debug(TAG, "dispose()")
+    }
+
+    companion object {
+        private const val TAG = "GameScreen"
+    }
 }
--- core/src/main/kotlin/com/goncharov/evgeny/chess/screens/SettingsScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/screens/SettingsScreen.kt
@@ -1,6 +1,32 @@
 package com.goncharov.evgeny.chess.screens
 
 import com.goncharov.evgeny.chess.base.BaseScreen
+import com.goncharov.evgeny.chess.utils.clearScreen
+import com.goncharov.evgeny.chess.utils.debug
 
 class SettingsScreen : BaseScreen() {
+
+    override fun show() {
+        debug(TAG, "show()")
+    }
+
+    override fun render(delta: Float) {
+        clearScreen()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        debug(TAG, "resize()")
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+        debug(TAG, "dispose()")
+    }
+
+    companion object {
+        private const val TAG = "SettingsScreen"
+    }
 }
