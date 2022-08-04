e3d3d248946198aef2e29875f5eb2cb7ba4352b6
Evgeny Goncharov <jtgn@yandex.ru>
2022-08-05 02:56:28 +0300

    реализация главного меню

--- core/src/main/kotlin/com/goncharov/evgeny/chess/consts/GameConfig.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/consts/GameConfig.kt
@@ -3,8 +3,8 @@ package com.goncharov.evgeny.chess.consts
 const val WINDOW_WIDTH = 1280
 const val WINDOW_HEIGHT = 720
 
-const val UI_WIDTH = 640f
-const val UI_HEIGHT = 360f
+const val UI_WIDTH = 984.6f
+const val UI_HEIGHT = 553.8f
 
 const val WORLD_WIDTH = 12.8f
 const val WORLD_HEIGHT = 7.2f
--- core/src/main/kotlin/com/goncharov/evgeny/chess/screens/MainMenuScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/screens/MainMenuScreen.kt
@@ -3,9 +3,10 @@ package com.goncharov.evgeny.chess.screens
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.viewport.FillViewport
 import com.goncharov.evgeny.chess.base.BaseScreen
@@ -32,21 +33,54 @@ class MainMenuScreen(
         val table = Table()
         table.background = uiSkin.getDrawable(BACKGROUND_PAPER_ID)
         table.setFillParent(true)
+        val image1 = Image(uiSkin, "line_1")
+        table.add(image1).padLeft(8.0f).padTop(33.0f)
+
+        val table2 = Table()
+        val image2 = Image(uiSkin, "line_8")
+        table2.add(image2)
+        val label = Label("Menu", uiSkin)
+        table2.add(label).spaceLeft(12.0f).spaceRight(12.0f)
+        val image3 = Image(uiSkin, "line_7")
+        table2.add(image3)
+        val image4 = Image(uiSkin, "line_2")
+        table2.add(image4).growX()
+        table.add(table2).padTop(23.0f).fillX()
+        val image5 = Image(uiSkin, "line_3")
+        table.add(image5).padLeft(-8.0f).padBottom(-31.0f)
+        table.row()
+
+        val image6 = Image(uiSkin, "line_4")
+        table.add(image6).fillY()
+
+        val table3 = Table()
         val textButtonStartGame = ImageTextButton(TEXT_PLAYER_GAME, uiSkin)
         textButtonStartGame.addListenerKtx(::startPlayerVsPlayerGame)
-        table.add(textButtonStartGame)
-        table.row()
+        table3.add(textButtonStartGame)
+        table3.row()
         val textButtonStartGameWithComputer = ImageTextButton(TEXT_PLAYER_WITH_COMPUTER, uiSkin)
         textButtonStartGameWithComputer.isDisabled = true
-        table.add(textButtonStartGameWithComputer)
-        table.row()
+        table3.add(textButtonStartGameWithComputer)
+        table3.row()
         val textButtonSettings = ImageTextButton(TEXT_SETTINGS, uiSkin)
         textButtonSettings.addListenerKtx(::startSettings)
-        table.add(textButtonSettings)
-        table.row()
+        table3.add(textButtonSettings)
+        table3.row()
         val textButtonExit = ImageTextButton(TEXT_EXIT, uiSkin)
         textButtonExit.addListenerKtx(::exitApplication)
-        table.add(textButtonExit)
+        table3.add(textButtonExit)
+        table.add(table3)
+
+        val image7 = Image(uiSkin, "line_4")
+        table.add(image7).fillY()
+
+        table.row()
+        val image8 = Image(uiSkin, "line_5")
+        table.add(image8).padRight(-7.0f)
+        val image9 = Image(uiSkin, "line_2")
+        table.add(image9).padLeft(-13.0f).padRight(-13.0f).padTop(8.0f).fillX()
+        val image10 = Image(uiSkin, "line_6")
+        table.add(image10).padRight(8.0f)
         stage.addActor(table)
     }
 
