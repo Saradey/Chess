c303cb13a7aade2ca8c5eea7a0005fc11e3cfa61
Evgeny Goncharov <jtgn@yandex.ru>
2022-08-05 16:05:52 +0300

    рефакторинг главного меню

--- core/src/main/kotlin/com/goncharov/evgeny/chess/consts/ResourcesId.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/consts/ResourcesId.kt
@@ -1,3 +1,11 @@
 package com.goncharov.evgeny.chess.consts
 
 const val BACKGROUND_PAPER_ID = "paper background"
+const val LINE_1_ID = "line_1"
+const val LINE_8_ID = "line_8"
+const val LINE_7_ID = "line_7"
+const val LINE_2_ID = "line_2"
+const val LINE_3_ID = "line_3"
+const val LINE_4_ID = "line_4"
+const val LINE_5_ID = "line_5"
+const val LINE_6_ID = "line_6"
