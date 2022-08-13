c62244450c216636d625ad64fb97cea4295e76a9
Evgeny Goncharov <jtgn@yandex.ru>
2022-08-13 20:22:08 +0300

    отрефачили SplashScreen

---
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/ui/SplashStage.kt
@@ -0,0 +1,50 @@
+package com.goncharov.evgeny.chess.ui
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.chess.consts.TEXT_LOGO
+import com.goncharov.evgeny.chess.navigation.NavigationKey
+import com.goncharov.evgeny.chess.navigation.Navigator
+
+class SplashStage(
+    private val navigator: Navigator,
+    private val uiSkin: Skin,
+    bach: SpriteBatch,
+    viewport: Viewport
+) : Stage(viewport, bach) {
+
+    private var labelLogo: Label? = null
+
+    init {
+        initUi()
+        initAction()
+    }
+
+    private fun initUi() {
+        val table = Table()
+        table.setFillParent(true)
+        labelLogo = Label(TEXT_LOGO, uiSkin)
+        labelLogo?.setAlignment(Align.center)
+        table.add(labelLogo)
+        addActor(table)
+    }
+
+    private fun initAction() {
+        labelLogo?.addAction(
+            Actions.sequence(
+                Actions.alpha(0f),
+                Actions.fadeIn(1f),
+                Actions.fadeOut(1f),
+                Actions.run {
+                    navigator.navigation(NavigationKey.MainMenuScreenKey)
+                }
+            )
+        )
+    }
+}
