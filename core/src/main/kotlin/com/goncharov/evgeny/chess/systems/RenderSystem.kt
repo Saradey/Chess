fbf9e5225d2ad19eb9621477e599c12412f959ab
Evgeny Goncharov <jtgn@yandex.ru>
2022-08-18 12:50:11 +0300

    фикс RenderSystem оптимизация

--- core/src/main/kotlin/com/goncharov/evgeny/chess/systems/RenderSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/chess/systems/RenderSystem.kt
@@ -4,6 +4,8 @@ import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.SortedIteratingSystem
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.chess.components.mappers.dragged
+import com.goncharov.evgeny.chess.components.mappers.game
 import com.goncharov.evgeny.chess.components.mappers.sprites
 import com.goncharov.evgeny.chess.consts.*
 import com.goncharov.evgeny.chess.utils.LayerComparator
@@ -13,8 +15,12 @@ class RenderSystem(
     private val batch: SpriteBatch
 ) : SortedIteratingSystem(renderFamily, LayerComparator()) {
 
+    private val gameEntity by lazy {
+        engine.getEntitiesFor(gameFamily).first()
+    }
+
     override fun processEntity(entity: Entity, deltaTime: Float) {
-        forceSort()
+        if(dragged[gameEntity].isDragged) forceSort()
         viewport.apply()
         batch.projectionMatrix = viewport.camera.combined
         batch.begin()
